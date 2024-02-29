e09c0492114e87e5084880f9f6cd68bed652043b
l4zy9uy <hoanghuudon02hp@gmail.com>
2024-02-29 21:05:40 +0700

    Complete Chapter 5

--- main/main.cpp
+++ main/main.cpp
@@ -7,10 +7,31 @@
 #include <string>
 #include "../include/Sphere.h"
 int main() {
-    Ray r(glm::vec4(0.0f, 0.0f, -5.0f, 1.0f), glm::vec4(0.0f, 0.0f, 1.0f, 0.0f));
-    std::shared_ptr<Sphere> s = std::make_shared<Sphere>();
-    s->setTransform(glm::scale(glm::identity<glm::mat4>(), glm::vec3(2.0f)));
-    auto xs = s->intersect(r);
-    std::cout << xs.getIntersections()[0].getT() << " " << xs.getIntersections()[1].getT() << "\n";
+    glm::vec4 ray_origin(0.0f, 0.0f, -5.0f, 1.0f);
+    float wall_z = 10.0f;
+    float wall_size = 7.0f;
+    int canvas_pixels = 100;
+    float pixel_size = wall_size / canvas_pixels;
+    float half = wall_size / 2;
+    Canvas canvas(canvas_pixels, canvas_pixels);
+    glm::vec3 color(1.0f, 0.0f, 0.0f);
+    Sphere s;
+    for(int i = 0; i < canvas_pixels; i++)
+    {
+        auto world_y = half - pixel_size * i;
+        for(int j = 0; j < canvas_pixels - 1; j++)
+        {
+            auto world_x = -half + pixel_size * j;
+            glm::vec4 position(world_x, world_y, wall_z, 1.0f);
+            Ray r(ray_origin, glm::normalize(position - ray_origin));
+            auto xs = s.intersect(r);
+            if(xs.hit().has_value())
+            {
+                canvas.writePixel(i, j, color);
+                std::cout << "i: " << i << " j: " << j << "\n";
+            }
+        }
+    }
+    canvas.canvas_to_ppm();
     return 0;
 }
